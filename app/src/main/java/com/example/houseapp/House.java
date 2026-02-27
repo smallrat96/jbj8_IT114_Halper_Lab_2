8e3035ea043be781ad1c0335c29337845ece01af
Jack <jdtvlogs10@gmail.com>
2026-02-27 13:50:16 -0500

    Submission of Lab_2 Jack Johnson jbj8 jack johnson Dr. Halper IT114

---
+++ app/src/main/java/com/example/houseapp/House.java
@@ -0,0 +1,61 @@
+package com.example.houseapp;
+
+public class House {
+    private String address;
+    private String city;
+    private String lotnum;
+    private float price;
+    private float ptax;
+    private float sqfoot;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getLotnum() {
+        return lotnum;
+    }
+
+    public void setLotnum(String lotnum) {
+        this.lotnum = lotnum;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public float getPtax() {
+        return ptax;
+    }
+
+    public void setPtax(float ptax) {
+        this.ptax = ptax;
+    }
+
+    public float getSqfoot() {
+        return sqfoot;
+    }
+
+    public void setSqfoot(float sqfoot) {
+        this.sqfoot = sqfoot;
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/houseapp/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.houseapp;
+
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import java.io.File;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+
+    }
+
+    public void displayHouses(View view) {
+        EditText et1 = (EditText) findViewById(R.id.edit_file);
+        TextView textView;
+        textView = findViewById(R.id.text_main);
+        String url = String.valueOf(et1.getText());
+
+        //The try statement to catch an imporperly formatted url or an empty file
+        try {
+            //URL Object
+            URL urlO = new URL(url);
+            //Buffered reader attachted to a inputstreamreader. The input stream reader reads the line to the buffered reader and goes line by line in the file
+            //to pull out information. I chose this setup for the convience of going line by line because each attribute of the house object is equal
+            BufferedReader reader = new BufferedReader(new InputStreamReader(urlO.openStream()));
+
+            House[] houseList = new House[3];
+            String line;
+            int lineCount = 0;
+            int passCount = 0;
+            for (int i = 0; i < 3; i++) {
+                houseList[i] = new House(); // instantiate all house objects
+            }
+            while ((line = reader.readLine()) != null) {  //this statement checks to see that the line that is grabbed is not recieved
+                //this while statement is essentially a nested for loop :D
+                lineCount++;
+                if (lineCount == 1) {
+                    houseList[passCount].setAddress(line);
+                    continue;
+                } else if (lineCount == 2) {
+                    houseList[passCount].setCity(line);
+                    continue;
+                } else if (lineCount == 3) {
+                    houseList[passCount].setLotnum(line);
+                    continue;
+                } else if (lineCount == 4) {
+                    houseList[passCount].setPrice(Float.parseFloat(line));
+                    continue;
+                } else if (lineCount == 5) {
+                    houseList[passCount].setPtax(Float.parseFloat(line));
+                    continue;
+                } else if (lineCount == 6) {
+
+                    houseList[passCount].setSqfoot(Integer.parseInt(line));
+                    lineCount = 0;
+                    passCount++;
+                    if (passCount > 2) {
+                        break;
+                    }
+                    continue;
+                    /*
+                    textView.setText(houseList[passCount].getAddress().concat("\n"));
+                    textView.append(houseList[passCount].getCity().concat("\n"));
+                    textView.append(houseList[passCount].getLotnum().concat("\n"));
+                    textView.append(String.format(Locale.US, "%f3",houseList[passCount].getPtax()).concat("\n"));
+                    textView.append(String.format(Locale.US, "%f3", houseList[passCount].getPrice()).concat("\n"));
+                    textView.append(String.valueOf(houseList[passCount].getSqfoot()).concat("\n")); */
+                }
+                /*
+                for (int i = 0; i < 3; i++) {
+
+                 */
+            }
+            textView.setText("");//Reset text view
+            double totalSqFt = 0;
+            for (int i = 0; i < 3; i++) {
+                textView.append(houseList[i].getAddress().concat("\n"));
+                textView.append(houseList[i].getCity().concat("\n"));
+                textView.append(houseList[i].getLotnum().concat("\n"));
+                textView.append(String.format(Locale.US, "%f3",houseList[i].getPtax()).concat("\n"));
+                textView.append(String.format(Locale.US, "%f3", houseList[i].getPrice()).concat("\n"));
+                textView.append(String.valueOf(houseList[i].getSqfoot()).concat("\n"));
+                totalSqFt += houseList[i].getSqfoot();
+            }
+            double avgSqFt = totalSqFt/3;
+            double weightedPtax = ((houseList[0].getSqfoot() * houseList[0].getPtax()) + (houseList[1].getSqfoot() * houseList[1].getPtax()) + (houseList[2].getSqfoot() * houseList[2].getPtax()) ) / totalSqFt;
+            textView.append("Average Square Feet: " + String.format(Locale.US, "%f3",avgSqFt) + "\n");
+            textView.append("This is the weighted property tax: " + String.format(Locale.US, "%f2",weightedPtax));
+
+
+
+
+
+        } catch (MalformedURLException e) { //exception handling for input/output and a malformed url exception
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
+
+
+
